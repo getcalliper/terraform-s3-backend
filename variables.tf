e31c23a54f95197f18cb8613090bb3297d8d07bf
Lawrence Hunt <huntlawrence@gmail.com>
2022-12-29 08:16:25 +0700

    add calliper variable defaults

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "bucket_prefix" {
   type        = string
   description = "A prefix applied to the S3 bucket created to ensure a unique name."
+  default     = "calliper"
 }
 
 variable "bucket_sse_algorithm" {
@@ -12,11 +13,15 @@ variable "bucket_sse_algorithm" {
 variable "workspaces" {
   type        = list(string)
   description = "A list of terraform workspaces that IAM Roles/Policy will be created for"
+  default     = ["dev", "prod"]
 }
 
 variable "assume_policy" {
   type        = map(string)
   description = "A map that allows you to specify additional AWS principles that will be added to the backend roles assume role policy"
 
-  default = {}
+  default = {
+    dev  = "arn:aws:iam::053474479217:root"
+    prod = "arn:aws:iam::959101439898:root"
+  }
 }
